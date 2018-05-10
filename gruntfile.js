8bdb10a32ae1120defc20312088e1c240d591962
Maciej Duda <maciej.duda1@gazeta.pl>
2018-05-10 13:47:22 +0200

    new page project started

---
+++ gruntfile.js
@@ -0,0 +1,42 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        sass: {
+            options: {
+                sourceMap: false
+            },
+            dist: {
+                files: {
+                    'css/style.css': 'sass/style.scss'
+                }
+            }
+        },
+        watch: {
+            files: 'sass/*.scss', 
+            tasks: ['sass'],
+                options: {
+                    spawn: false,
+                }
+        },
+        browserSync: {
+            dev: {
+                bsFiles: {
+                    src : [
+                        'css/*.css',
+                        './*.html'
+                    ]
+                },
+                options: {
+                    watchTask: true,
+                    server: './.'
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-browser-sync');
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('default', ['browserSync', 'watch']);
+  
+  };
---
+++ scripts/index.js
@@ -0,0 +1,43 @@
+$('#fullpage').fullpage();
+//on Button menu
+$('#on-click-menu').on('click', function() {
+    $('nav').slideToggle('slow');
+});
+// on Click scroll 
+$('.scroll-button').mouseenter(function(){
+    $('.scroll-line').animate({
+        width: '150px',
+        margin: '0 0 0 20px'
+    }, 1000)
+});
+
+$('.scroll-button').mouseleave(function(){
+    $('.scroll-line').animate({
+        width: '50px',
+        margin: '0 0 0 50px'
+    }, 1000)
+});
+
+//About section
+$('#bio-life-text').hide();
+$('#bio-skills-text').hide();
+$('#bio-work-text').hide();
+
+$('.bio-work').on('click', function() {
+    $('#bio-work-text').toggle();
+    $('#bio-skills-text').hide();
+    $('#bio-life-text').hide();
+});
+
+$('.bio-skills').on('click', function() {
+    $('#bio-work-text').hide();
+    $('#bio-skills-text').toggle();
+    $('#bio-life-text').hide();
+});
+
+$('.bio-life').on('click', function() {
+    $('#bio-work-text').hide();
+    $('#bio-skills-text').hide();
+    $('#bio-life-text').toggle();
+});
+
